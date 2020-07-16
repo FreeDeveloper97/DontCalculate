670405f893af85fdd1a8e00e3a0145877697073b
강민상 <freedeveloper97@gmail.com>
2020-07-16 12:06:13 +0900

    7/16 메인화면 1차 완성판, 다크모드 이미지 지원

--- Calculator Plus/ViewController.swift
+++ Calculator Plus/ViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-//    @IBOutlet var Button_1: UIButton!
-//    @IBOutlet var Button_2: UIButton!
-//    @IBOutlet var Button_3: UIButton!
-//    @IBOutlet var Button_4: UIButton!
-//    @IBOutlet var Button_5: UIButton!
-//    @IBOutlet var Button_6: UIButton!
-//    @IBOutlet var Button_7: UIButton!
-//    @IBOutlet var Button_8: UIButton!
-//    @IBOutlet var Button_9: UIButton!
+    @IBOutlet var Button_1: UIButton!
+    @IBOutlet var Button_2: UIButton!
+    @IBOutlet var Button_3: UIButton!
+    @IBOutlet var Button_4: UIButton!
+    @IBOutlet var Button_5: UIButton!
+    @IBOutlet var Button_6: UIButton!
+    @IBOutlet var Button_7: UIButton!
+    @IBOutlet var Button_8: UIButton!
+    @IBOutlet var Button_9: UIButton!
     
     
     let brown = UIColor(named: "Dynamic_brown")
@@ -29,43 +29,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
 //        Button_1.layer.cornerRadius = 8
 //        Button_1.clipsToBounds = true
-//
-//        Button_2.layer.cornerRadius = 8
-//        Button_2.clipsToBounds = true
-//
-//        Button_3.layer.cornerRadius = 8
-//        Button_3.clipsToBounds = true
-//
-//        Button_4.layer.cornerRadius = 8
-//        Button_4.clipsToBounds = true
-//
-//        Button_5.layer.cornerRadius = 8
-//        Button_5.clipsToBounds = true
-//
-//        Button_6.layer.cornerRadius = 8
-//        Button_6.clipsToBounds = true
-//
-//        Button_7.layer.cornerRadius = 8
-//        Button_7.clipsToBounds = true
-//
-//        Button_8.layer.cornerRadius = 8
-//        Button_8.clipsToBounds = true
-//
-//        Button_9.layer.cornerRadius = 8
-//        Button_9.clipsToBounds = true
 
-        //66, 119, 214
+        Button_1.setImage(UIImage(named: "test_1"), for: .normal)
+        Button_2.setImage(UIImage(named: "test_2"), for: .normal)
+        Button_3.setImage(UIImage(named: "test_3"), for: .normal)
+        Button_4.setImage(UIImage(named: "test_4"), for: .normal)
+        Button_5.setImage(UIImage(named: "test_5"), for: .normal)
+        Button_6.setImage(UIImage(named: "test_6"), for: .normal)
+        Button_7.setImage(UIImage(named: "test_7"), for: .normal)
+        Button_8.setImage(UIImage(named: "test_8"), for: .normal)
+        Button_9.setImage(UIImage(named: "test_9"), for: .normal)
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        let activity_1_ViewController = segue.destination as! Activity_1_ViewController
-//        if segue.identifier == "Button_1"
-//        {
-//
-//        }
-//    }
-
 
 }
--- Calculator Plus/View_1_1_ViewController.swift
+++ Calculator Plus/View_1_1_ViewController.swift
@@ -10,25 +10,30 @@ import UIKit
 
 class View_1_1_ViewController: UIViewController {
     
-    @IBOutlet var View_1: UIView!
-    @IBOutlet var Input_money: UITextField!
-    @IBOutlet var Button_plus_outlet: UIButton!
-    @IBOutlet var Text_money: UILabel!
-    @IBOutlet var Text_n: UITextField!
-    @IBOutlet var Button_Result_outlet: UIButton!
-    @IBOutlet var Result_1: UILabel!
-    @IBOutlet var View_Result: UIView!
-    @IBOutlet var Button_kakao_outlet: UIButton!
-//    @IBOutlet var View_2: UIView!
-    @IBOutlet var Button_detail_outlet: UIButton!
-    @IBOutlet var Text_detail: UILabel!
-    @IBOutlet var Text_show: UILabel!
+//    @IBOutlet var View_1: UIView!
+//    @IBOutlet var Input_money: UITextField!
+//    @IBOutlet var Button_plus_outlet: UIButton!
+//    @IBOutlet var Text_money: UILabel!
+//    @IBOutlet var Text_n: UITextField!
+//    @IBOutlet var Button_Result_outlet: UIButton!
+//    @IBOutlet var Result_1: UILabel!
+//    @IBOutlet var View_Result: UIView!
+//    @IBOutlet var Button_kakao_outlet: UIButton!
+////    @IBOutlet var View_2: UIView!
+//    @IBOutlet var Button_detail_outlet: UIButton!
+//    @IBOutlet var Text_detail: UILabel!
+//    @IBOutlet var Text_show: UILabel!
+//
+//    @IBOutlet var View_bank: UIView!
+//    @IBOutlet var Text_bank: UITextField!
+//
+//    @IBOutlet var Text_bank_show: UILabel!
     
-    @IBOutlet var View_bank: UIView!
-    @IBOutlet var Text_bank: UITextField!
-    
-    @IBOutlet var Text_bank_show: UILabel!
-    
+    @IBOutlet var Button_Plus: UIButton!
+    @IBOutlet var Button_Result: UIButton!
+    @IBOutlet var View_Sum: UIView!
+    @IBOutlet var Button_Detail: UIButton!
+    @IBOutlet var Button_Kakao: UIButton!
     
     
     var InputMoney = ""
@@ -43,170 +48,174 @@ class View_1_1_ViewController: UIViewController {
     let kakao = UIColor(named: "Dynamic_kakao")
     let button = UIColor(named: "Dynamic_button")
     
+    
     override func viewDidLoad() {
         
-        View_1.layer.cornerRadius = 18
-//        View_1.layer.borderWidth = 2
-//        View_1.layer.borderColor = brown?.cgColor
+//        View_1.layer.cornerRadius = 18
+//
+//        Button_plus_outlet.layer.cornerRadius = 13
+//
+//        Button_Result_outlet.layer.cornerRadius = 13
+//
+//        View_Result.layer.cornerRadius = 13
+//
+//        View_bank.layer.cornerRadius = 18
+//
+//        Button_kakao_outlet.layer.cornerRadius = 13
+//
+//        Button_detail_outlet.layer.cornerRadius = 13
+//
+//        Input_money.keyboardType = .numberPad
+//        Text_n.keyboardType = .numberPad
+//
+//        Input_money.attributedPlaceholder = NSAttributedString(string: "금액입력후 +버튼", attributes: [NSAttributedString.Key.foregroundColor: brown?.cgColor])
+//
+//        Text_bank.attributedPlaceholder = NSAttributedString(string: "은행, 계좌번호 입력", attributes: [NSAttributedString.Key.foregroundColor: brown?.cgColor])
+//
+//        self.hideKeyboard()
         
-        Button_plus_outlet.layer.cornerRadius = 13
-//        Button_plus_outlet.layer.borderWidth = 2
-//        Button_plus_outlet.layer.borderColor = button?.cgColor
         
-        Button_Result_outlet.layer.cornerRadius = 13
-//        Button_Result_outlet.layer.borderWidth = 2
-//        Button_Result_outlet.layer.borderColor = button?.cgColor
         
-        View_Result.layer.cornerRadius = 13
-//        View_Result.layer.borderWidth = 2
-//        View_Result.layer.borderColor = brown?.cgColor
+        Button_Plus.layer.cornerRadius = Button_Plus.layer.frame.size.width/2
+//        Button_Plus.clipsToBounds = true
         
-        View_bank.layer.cornerRadius = 18
-//        View_bank.layer.borderWidth = 2
-//        View_bank.layer.borderColor = brown?.cgColor
-
-        Button_kakao_outlet.layer.cornerRadius = 13
-//        Button_kakao_outlet.layer.borderWidth = 2
-//        Button_kakao_outlet.layer.borderColor = kakao?.cgColor
+        Button_Result.layer.cornerRadius = Button_Result.layer.frame.size.width/2
+//        Button_Result.clipsToBounds = true
         
-        Button_detail_outlet.layer.cornerRadius = 13
-//        Button_detail_outlet.layer.borderWidth = 2
-//        Button_detail_outlet.layer.borderColor = button?.cgColor
+        View_Sum.layer.cornerRadius = 8
         
-        Input_money.keyboardType = .numberPad
-        Text_n.keyboardType = .numberPad
+        Button_Detail.layer.cornerRadius = 8
+        Button_Detail.clipsToBounds = true
         
-        Input_money.attributedPlaceholder = NSAttributedString(string: "금액입력후 +버튼", attributes: [NSAttributedString.Key.foregroundColor: brown?.cgColor])
+        Button_Kakao.layer.cornerRadius = 8
+        Button_Kakao.clipsToBounds = true
         
-        Text_bank.attributedPlaceholder = NSAttributedString(string: "은행, 계좌번호 입력", attributes: [NSAttributedString.Key.foregroundColor: brown?.cgColor])
-        
-        self.hideKeyboard()
         super.viewDidLoad()
     }
     
-    @IBAction func Button_plus(_ sender: UIButton) {
-        InputMoney = Input_money.text!
-        if(InputMoney != "")
-        {
-            MONEY += Int(InputMoney)!
-            Input_money.text = ""
-            Text_money.text = "누적금액 : " + String(MONEY) + "원"
-            if(DETAIL == "")
-            {
-                DETAIL = String(InputMoney) + " 원"
-            }
-            else if (DETAIL != "")
-            {
-                DETAIL += "\n" + String(InputMoney) + " 원"
-            }
-        }
-    }
+//    @IBAction func Button_plus(_ sender: UIButton) {
+//        InputMoney = Input_money.text!
+//        if(InputMoney != "")
+//        {
+//            MONEY += Int(InputMoney)!
+//            Input_money.text = ""
+//            Text_money.text = "누적금액 : " + String(MONEY) + "원"
+//            if(DETAIL == "")
+//            {
+//                DETAIL = String(InputMoney) + " 원"
+//            }
+//            else if (DETAIL != "")
+//            {
+//                DETAIL += "\n" + String(InputMoney) + " 원"
+//            }
+//        }
+//    }
     
     
     
-    @IBAction func Button_Result(_ sender: UIButton) {
-        //1. string 가져오기
-        N = Text_n.text!
-        if(check())
-        {
-            kakao_print = "총 사용금액 : " + String(MONEY) + " 원"
-            kakao_print += "\n더치페이 인원수 : " + N + " 명"
-
-            RESULT = MONEY / Int(N)!
-            
-            inputComma()
-            Result_1.text = String(RESULT_COMMA) + " 원"
-//            Result_1.text = String(RESULT) + " 원"
-            Text_bank_show.text = Text_bank.text!
-            
-            
-            kakao_print += "\n\n더치페이 결과 : " + String(RESULT_COMMA) + " 원"
-//            kakao_print += "\n\n더치페이 결과 : " + String(RESULT) + " 원"
-            kakao_print += "\n계좌 : " + Text_bank.text!
-        }
-        
-    }
+//    @IBAction func Button_Result(_ sender: UIButton) {
+//        //1. string 가져오기
+//        N = Text_n.text!
+//        if(check())
+//        {
+//            kakao_print = "총 사용금액 : " + String(MONEY) + " 원"
+//            kakao_print += "\n더치페이 인원수 : " + N + " 명"
+//
+//            RESULT = MONEY / Int(N)!
+//
+//            inputComma()
+//            Result_1.text = String(RESULT_COMMA) + " 원"
+////            Result_1.text = String(RESULT) + " 원"
+//            Text_bank_show.text = Text_bank.text!
+//
+//
+//            kakao_print += "\n\n더치페이 결과 : " + String(RESULT_COMMA) + " 원"
+////            kakao_print += "\n\n더치페이 결과 : " + String(RESULT) + " 원"
+//            kakao_print += "\n계좌 : " + Text_bank.text!
+//        }
+//
+//    }
     
-    @IBAction func Button_detail(_ sender: UIButton) {
-        if(RESULT != 0)
-        {
-            Text_show.text = "상세내역 :"
-            Text_detail.text = "\n" + DETAIL
-            kakao_print += "\n\n상세내역\n" + DETAIL
-        }
-    }
+//    @IBAction func Button_detail(_ sender: UIButton) {
+//        if(RESULT != 0)
+//        {
+//            Text_show.text = "상세내역 :"
+//            Text_detail.text = "\n" + DETAIL
+//            kakao_print += "\n\n상세내역\n" + DETAIL
+//        }
+//    }
     
     
-    @IBAction func Button_kakao(_ sender: UIButton) {
-        if kakao_print != ""
-        {
-
-            let template = KMTTextTemplate { (KMTTextTemplateBuilder) in
-                KMTTextTemplateBuilder.text = self.kakao_print
-                //KMTTextTemplateBuilder.buttonTitle = "Title"
-                KMTTextTemplateBuilder.link = KMTLinkObject(builderBlock: { (linkBuilder) in
-                    linkBuilder.mobileWebURL = URL(string: "https://developers.kakao.com")
-                })
-            }
-
-            // 서버에서 콜백으로 받을 정보
-            let serverCallbackArgs = ["user_id": "FDEE",
-                                      "product_id": "415849"]
-
-            // 카카오링크 실행
-            KLKTalkLinkCenter.shared().sendDefault(with: template, serverCallbackArgs: serverCallbackArgs, success: { (warningMsg, argumentMsg) in
-
-                // 성공
-                print("warning message: \(String(describing: warningMsg))")
-                print("argument message: \(String(describing: argumentMsg))")
-
-            }, failure: { (error) in
-
-                // 실패
-                UIAlertController.showMessage(error.localizedDescription)
-                print("error \(error)")
-            })
-
-        }
-    }
+//    @IBAction func Button_kakao(_ sender: UIButton) {
+//        if kakao_print != ""
+//        {
+//
+//            let template = KMTTextTemplate { (KMTTextTemplateBuilder) in
+//                KMTTextTemplateBuilder.text = self.kakao_print
+//                //KMTTextTemplateBuilder.buttonTitle = "Title"
+//                KMTTextTemplateBuilder.link = KMTLinkObject(builderBlock: { (linkBuilder) in
+//                    linkBuilder.mobileWebURL = URL(string: "https://developers.kakao.com")
+//                })
+//            }
+//
+//            // 서버에서 콜백으로 받을 정보
+//            let serverCallbackArgs = ["user_id": "FDEE",
+//                                      "product_id": "415849"]
+//
+//            // 카카오링크 실행
+//            KLKTalkLinkCenter.shared().sendDefault(with: template, serverCallbackArgs: serverCallbackArgs, success: { (warningMsg, argumentMsg) in
+//
+//                // 성공
+//                print("warning message: \(String(describing: warningMsg))")
+//                print("argument message: \(String(describing: argumentMsg))")
+//
+//            }, failure: { (error) in
+//
+//                // 실패
+//                UIAlertController.showMessage(error.localizedDescription)
+//                print("error \(error)")
+//            })
+//
+//        }
+//    }
     
-    @IBAction func Button_reset(_ sender: UIButton) {
-        InputMoney = ""
-        MONEY = 0
-        N = ""
-        RESULT = 0
-        kakao_print = ""
-        DETAIL = ""
-        Input_money.text = ""
-        Text_money.text = "누적금액 : 0원"
-        Text_n.text = ""
-        Result_1.text = ""
-        Text_detail.text = ""
-        Text_show.text = ""
-        Text_bank.text = ""
-        Text_bank_show.text = ""
-    }
+//    @IBAction func Button_reset(_ sender: UIButton) {
+//        InputMoney = ""
+//        MONEY = 0
+//        N = ""
+//        RESULT = 0
+//        kakao_print = ""
+//        DETAIL = ""
+//        Input_money.text = ""
+//        Text_money.text = "누적금액 : 0원"
+//        Text_n.text = ""
+//        Result_1.text = ""
+//        Text_detail.text = ""
+//        Text_show.text = ""
+//        Text_bank.text = ""
+//        Text_bank_show.text = ""
+//    }
     
-    func check() -> Bool
-    {
-        if Text_money.text == "누적금액 : 0원"
-        {
-            return false
-        }
-        else if N == ""
-        {
-            return false
-        }
-        return true
-    }
-    
-    func inputComma()
-    {
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-        
-        RESULT_COMMA = numberFormatter.string(from:NSNumber(value: RESULT))!
-    }
+//    func check() -> Bool
+//    {
+//        if Text_money.text == "누적금액 : 0원"
+//        {
+//            return false
+//        }
+//        else if N == ""
+//        {
+//            return false
+//        }
+//        return true
+//    }
+//
+//    func inputComma()
+//    {
+//        let numberFormatter = NumberFormatter()
+//        numberFormatter.numberStyle = .decimal
+//
+//        RESULT_COMMA = numberFormatter.string(from:NSNumber(value: RESULT))!
+//    }
     
     
 }
